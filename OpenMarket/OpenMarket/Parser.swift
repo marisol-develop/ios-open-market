4469225494d7c05e1bc752ddcaaca98daa87e65b
Eddy <kimkyunghun3@naver.com>
2022-05-11 00:29:00 +0900

    [refactor]: decode 타입을 제네릭으로 변경

--- OpenMarket/OpenMarket/Parser.swift
+++ OpenMarket/OpenMarket/Parser.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-struct Parser {
-    func decode() throws -> [Products] {
+struct Parser<T: Decodable> {
+    func decode() throws -> T {
         let jsonDecoder = JSONDecoder()
         
         guard let productData = NSDataAsset(name: "products") else {
             throw DecodingError.notFoundFile
         }
         
-        guard let products = try? jsonDecoder.decode([Products].self, from: productData.data) else {
+        guard let products = try? jsonDecoder.decode(T.self, from: productData.data) else {
             throw DecodingError.failedDecoding
         }
         
