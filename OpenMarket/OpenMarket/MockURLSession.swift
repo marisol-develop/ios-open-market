9757d34178082792109ca8f32084e557adf7c160
marisol <marisol.develop@gmail.com>
2022-05-10 11:55:08 +0900

    [feat]: MockURLSession 생성

--- OpenMarket/OpenMarket/MockURLSession.swift
+++ OpenMarket/OpenMarket/MockURLSession.swift
@@ -18,3 +18,36 @@ final class MockURLSessionDataTask: URLSessionDataTask {
         completion()
     }
 }
+
+final class MockURLSession: URLSessionProtocol {
+    var isRequestSuccess: Bool
+    var sessionDataTask: MockURLSessionDataTask?
+    
+    init(isRequestSucceses: Bool = true) {
+        self.isRequestSuccess = isRequestSucceses
+    }
+    
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        let successResponse = HTTPURLResponse(url: url,
+                                              statusCode: 200,
+                                              httpVersion: "2",
+                                              headerFields: nil)
+        let failureResponse = HTTPURLResponse(url: url,
+                                              statusCode: 404,
+                                              httpVersion: "2",
+                                              headerFields: nil)
+        let sessionDataTask = MockURLSessionDataTask()
+        
+        if isRequestSuccess {
+            sessionDataTask.completion = {
+                completionHandler(MockData().data, successResponse, nil)
+            }
+        } else {
+            sessionDataTask.completion = {
+                completionHandler(nil, failureResponse, nil)
+            }
+        }
+
+        return sessionDataTask
+    }
+}
