2d56a8bf56eab7bb600b99c0c160006aa3fceea0
Eddy <kimkyunghun3@naver.com>
2022-05-17 17:48:46 +0900

    [feat]: stackView 생성하는 메서드 구현 및 필요한 StackView 생성

--- OpenMarket/OpenMarket/Controller/CollectionViewCell.swift
+++ OpenMarket/OpenMarket/Controller/CollectionViewCell.swift
@@ -16,6 +16,20 @@ final class CollectionViewCell: UICollectionViewCell {
     var bargainPrice: UILabel = UILabel()
     var stock: UILabel = UILabel()
     
+    private lazy var priceStackView = makeStackView(axis: .horizontal, alignment: .leading, distribution: .equalCentering, spacing: 5)
+    private lazy var productStackView = makeStackView(axis: .vertical, alignment: .fill, distribution: .fill, spacing: 5)
+    private lazy var productWithImageStackView = makeStackView(axis: .horizontal, alignment: .fill, distribution: .fill, spacing: 5)
+    private lazy var accessoryStackView = makeStackView(axis: .horizontal, alignment: .top, distribution: .fill, spacing: 5)
     
-    
+    private func makeStackView(axis: NSLayoutConstraint.Axis, alignment: UIStackView.Alignment, distribution: UIStackView.Distribution, spacing: CGFloat) -> UIStackView {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = axis
+        stackView.alignment = alignment
+        stackView.distribution = distribution
+        stackView.spacing = spacing
+        
+        return stackView
+    }
 }
