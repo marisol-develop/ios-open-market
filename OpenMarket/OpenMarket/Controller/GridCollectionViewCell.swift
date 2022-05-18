0bd93b22580d4bd51c86a581657f3efc04777f76
marisol <marisol.develop@gmail.com>
2022-05-18 11:00:45 +0900

    [add]: GridCollectionViewCell 파일 생성

---
+++ OpenMarket/OpenMarket/Controller/GridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GridCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Eddy, marisol on 2022/05/18.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+}
