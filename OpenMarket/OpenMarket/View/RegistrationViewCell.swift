787ba5055e99c70116691190c548617e8ca5de68
marisol <marisol.develop@gmail.com>
2022-05-25 18:38:50 +0900

    [feat]: cell이 초기화될 때 button 생성하도록 기능 구현

--- OpenMarket/OpenMarket/View/RegistrationViewCell.swift
+++ OpenMarket/OpenMarket/View/RegistrationViewCell.swift
@@ -9,21 +9,32 @@ import UIKit
 
 final class RegistrationViewCell: UICollectionViewCell {
     static let identifier = "RegistrationViewCell"
-    let imageView = UIImageView()
+    let button = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(imageView)
+        contentView.addSubview(button)
         makeProductImage()
     }
     
     required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        contentView.addSubview(button)
+        makeProductImage()
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        button.setImage(nil, for: .normal)
+        makeProductImage()
+    }
+    
     func makeProductImage() {
-        imageView.image = UIImage(systemName: "swift")
-        imageView.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
-        imageView.isUserInteractionEnabled = true
+        button.setTitle("+", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.backgroundColor = .systemGray3
+        button.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.width)
     }
 }
